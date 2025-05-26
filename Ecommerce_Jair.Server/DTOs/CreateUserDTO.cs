e52f90a038a5d4f9c1e8c8e09e51b1d27ba54573
JairGZZ <jairprox1234@gmail.com>
2025-05-25 20:10:37 -0500

    se creo el dto CreateUserDTO

---
+++ Ecommerce_Jair.Server/DTOs/CreateUserDTO.cs
@@ -0,0 +1,16 @@
+// DTOs/UserCreateDto.cs
+
+
+
+namespace Ecommerce_Jair.Server.DTOs{
+
+    public class CreateUserDTO
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string? PhoneNumber { get; set; }
+    }
+
+}
