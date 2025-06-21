1dcdea1b238234969f5e5d05953f165fb37a4ddf
JairGZZ <jairprox1234@gmail.com>
2025-06-20 23:57:31 -0500

    feat: add email service

---
+++ Ecommerce_Jair.Server/Services/implementations/EmailService.cs
@@ -0,0 +1,38 @@
+﻿using Ecommerce_Jair.Server.Services.Interfaces;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace Ecommerce_Jair.Server.Services.implementations
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string recipientEmail, string subject, string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(new MailboxAddress("Ecommerce_Jair", _configuration["EmailSettings:SenderEmail"]));
+            email.To.Add(new MailboxAddress("", recipientEmail));
+            email.Subject = subject;
+
+            var builder = new BodyBuilder { HtmlBody = body };
+            email.Body = builder.ToMessageBody();
+
+            using var smtp = new SmtpClient(); 
+            await smtp.ConnectAsync(
+                _configuration["EmailSettings:SmtpServer"],
+                int.Parse(_configuration["EmailSettings:SmtpPort"]),
+                MailKit.Security.SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(
+                _configuration["EmailSettings:SenderEmail"],
+                _configuration["EmailSettings:SenderPassword"]);
+            await smtp.SendAsync(email);
+            await smtp.DisconnectAsync(true);
+        }
+    }
+}
