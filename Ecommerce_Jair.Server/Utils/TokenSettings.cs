f18b76e31e092e9f725bb4323312a01545fdf28c
JairGZZ <jairprox1234@gmail.com>
2025-06-25 01:56:41 -0500

    feat add utils for user tokens

---
+++ Ecommerce_Jair.Server/Utils/TokenSettings.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace Ecommerce_Jair.Server.Utils
+{
+    public class TokenSettings
+    {
+        public string SecretKey { get; set; } = null!;
+        public string Issuer { get; set; } = null!;
+        public string Audience { get; set; } = null!;
+        public int ExpiryMinutes { get; set; }
+    }
+}
---
+++ Ecommerce_Jair.Server/Utils/TokenUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+
+namespace Ecommerce_Jair.Server.Utils
+{
+    public static class TokenUtils
+    {
+
+        public static string GenerateSecureToken(int length = 32)
+    {
+        var randomBytes = new byte[length];
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(randomBytes);
+        }
+        return Convert.ToBase64String(randomBytes);
+    }
+
+}
+}
